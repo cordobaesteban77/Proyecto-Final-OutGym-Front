4f10633179b0194cf09dc9cc051845b61d919cbc
Esteban <cordobaesteban77@gmail.com>
2025-07-21 04:27:20 -0300

    empiezo el diseño del navbar

---
+++ src/components/Componente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Componente = () => {
+  return (
+    <div>
+        <h1>hola</h1>
+    </div>
+  )
+}
+
+export default Componente
---
+++ src/components/NavbarApp.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Link, NavLink} from "react-router-dom"
+
+const NavbarApp = () => {
+     const usuario = JSON.parse(localStorage.getItem("user")) || null
+  return (
+    <nav className="navbar navbar-expand-lg nav">
+  <div className="container-fluid">
+    <Link className="navbar-brand text-light" to="/">Logo</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink className={({isActive}) => isActive ? "nav-link color-avtivo" : "nav-link text-light"} aria-current="page" to="/">Home</NavLink>
+        </li>
+      </ul>
+        {
+            usuario ? <li className="nav-item ms-auto ms-0">
+            <NavLink className="nav-link color-avtivo ps-0" to="#">Cerrar sesión</NavLink>
+            </li> : 
+            <li className="nav-item ms-auto ms-0">
+            <NavLink className="nav-link color-avtivo ps-0" to="#">Iniciar sesión</NavLink>
+            </li>
+        }
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default NavbarApp
