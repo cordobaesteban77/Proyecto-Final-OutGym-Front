2da74a57b8c6f32ad8f80ce66fecd9f883c3d985
Esteban <cordobaesteban77@gmail.com>
2025-08-02 19:33:55 -0300

    corrijo error de importacion en el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homepage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
