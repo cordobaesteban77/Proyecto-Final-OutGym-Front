9be04ee5cf610d89a2a37085a0ffc05acf206bdd
Daniel <danisuarze@gmail.com>
2025-08-04 12:40:52 -0300

    reparo errores de integracion - video del home - duplicaciones en las importaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-import Footer from './components/footer.jsx'
-
-
-  
-import SobreNosotros from './components/Sobrenosotros.jsx'
-
-
-  
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import Footer from "./components/footer.jsx";
+import SobreNosotros from "./components/Sobrenosotros.jsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NotFoundScreen from "./pages/NotFoundScreen";
 import LoginScreen from "./pages/LoginScreen";
 import NavbarApp from "./components/NavbarApp";
-import Footer from './components/footer.jsx';
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <NavbarApp />
+        <NavbarApp />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginScreen />} />
           <Route path="*" element={<NotFoundScreen />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-
     </>
   );
 };
