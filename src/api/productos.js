0fafa0c54cab0cc8e8cfc091e2f27711b6831ba5
cristian <cristiancabre10@gmail.com>
2025-08-07 14:09:09 -0300

    se creo tabla para visualizar los productos, con boton de agregar nuevo producto, con su formulario, boton de habilitar/deshabilitar plan, boton de eliminar que abre un formulario con los campos llenos para modificar, y el boton eliminar conectados con los metodos get,post,put y delete del back

---
+++ src/api/productos.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3001/api/productos";
+
+export const fetchProductos = async () => {
+  const { data } = await axios.get(API_URL + "/");
+  return data.productos;
+};
+
+export const fetchProductoPorId = async (id) => {
+  const { data } = await axios.get(`${API_URL}/${id}`);
+  return data.producto;
+};
+
+export const crearProducto = async (nuevoProducto) => {
+  const { data } = await axios.post(API_URL, nuevoProducto);
+  console.log("Respuesta al crear producto:", data); // DEBUG
+  return data; // Debe tener msg e idProducto
+};
+
+export const subirImagenProducto = async (idProducto, file) => {
+  const formData = new FormData();
+  formData.append("imagen", file);
+
+  const { data } = await axios.put(`${API_URL}/addEditImage/${idProducto}`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return data;
+};
+
+export const editarProducto = async (id, producto) => {
+  const { data } = await axios.put(`${API_URL}/${id}`, producto);
+  return data;
+};
+
+export const eliminarProducto = async (id) => {
+  const { data } = await axios.delete(`${API_URL}/${id}`);
+  return data;
+};
+
+export const toggleEstadoProducto = async (idProducto) => {
+  const { data } = await axios.put(`${API_URL}/changeState/${idProducto}`);
+  return data;
+};
