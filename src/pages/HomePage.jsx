655d86215a23ec1743c1331114c17015d089d01a
Córdoba Esteban Augusto <cordobaesteban77@gmail.com>
2025-08-07 20:03:48 -0300

    se cambia nombre de archivo de modal membresia

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Parallaxbk from "../components/parallax/parallaxbk";
 import CardHome from "../components/card/Cards";
 import WeatherWidget from "../components/clima/OpenWeatherMap";
 import GymJumbotron from "../components/jumbotron/GymJumbotron";
-import MembresiaModal from "../components/membresia/membresiaModal";
+import MembresiaModal from "../components/membresia/MembresiaModal";
 import PlanesC from "../components/planes/PlanesC";
 
 const HomePage = () => {
